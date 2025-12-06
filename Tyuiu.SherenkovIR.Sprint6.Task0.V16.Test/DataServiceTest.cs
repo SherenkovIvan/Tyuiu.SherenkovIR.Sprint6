549337504959b014f73386d5b1d4e712d5731dea
Sherenkov Ivan <sherenkov12@gmail.com>
2025-12-06 15:37:59 +0500

    Add project files.

---
+++ Tyuiu.SherenkovIR.Sprint6.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SherenkovIR.Sprint6.Task0.V16.Lib;
+namespace Tyuiu.SherenkovIR.Sprint6.Task0.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Calc()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 9.333;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
